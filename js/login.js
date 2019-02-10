d57d1a72130c732a1b379e2d2fd5cf64ec7d93de
unknown <abdxax@gmail.com>
2019-02-10 22:59:23 +0300

    S

--- js/login.js
+++ js/login.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 	// body...
+  var hie=window.innerHeight;
+  //window.innerHeight;
+ /*if(hie<=812){
+   $(".back").css('height',0);
+
+  $(".vx").css('height',hie);
+ }
+ else{
+   $(".back").css('height',hie);
+
+  //$(".vx").css('height',hie);
+ }*/
+  $(".back").css('height',hie);
 	$("#ar").click(function(){
  document.getElementById("dire").dir="rtl";
   document.getElementById("title").innerHTML="نظام سجلات الطلاب ";
   document.getElementById("user").placeholder = "معرف المستخدم";
   document.getElementById("pass").placeholder = "كلمة المرور";
-  document.getElementById("rmber").placeholder = "تذكرني";
+  document.getElementById("rmber").innerHTML = "تذكرني";
+  $("#rmber").css('margin-right',"15px");
   document.getElementById("but").value = "تسجيل دخول ";
  
 });
@@ -14,7 +28,14 @@ $(document).ready(function(){
   document.getElementById("title").innerHTML="Student Information System";
   document.getElementById("user").placeholder = "User ID";
   document.getElementById("pass").placeholder = "Password";
-  document.getElementsByTagName("label").placeholder = "Remaber";
+  document.getElementById("rmber").innerHTML = "Remaber";
+   $("#rmber").css('margin-left',"10px");
   document.getElementById("but").value = "Login";
 });
+});
+
+// for loading 
+$(window).one("load",function(){
+
+$(".loading").fadeOut(5000);
 });
